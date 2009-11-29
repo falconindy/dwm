566cfde38d5d3d246221c243f3e56f9c5d9b93b1
Dave Reisner <haruko@quake.localdomain>
2009-11-29 10:12:14 -0500

    add window rule, reorder by tag number

--- config.h
+++ config.h
@@ -17,13 +17,14 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const char *tags[] = { "web", "term", "pidgin", "vbox", "misc" };
 
 static const Rule rules[] = {
-	/* class         instance    title       tags mask     isfloating   monitor */
-	{ "chrome",      NULL,       NULL,       1 << 0,            False,       -1 },
-	{ "Gimp",        NULL,       NULL,       1 << 4,            True,        -1 },
-	{ "Pidgin",      NULL,       NULL,       1 << 2,            True,        -1 },
-	{ "mplayer",     NULL,       NULL,       1 << 4,            True,        -1 },
-	{ "URxvt",       NULL,       NULL,       1 << 1,            False,       -1 },
-	{ "VirtualBox",  NULL,       NULL,       1 << 3,            True,        -1 },
+	/* class                 instance    title       tags mask     isfloating   monitor */
+	{ "chrome",              NULL,       NULL,       1 << 0,            False,       -1 },
+	{ "URxvt",               NULL,       NULL,       1 << 1,            False,       -1 },
+	{ "Pidgin",              NULL,       NULL,       1 << 2,            True,        -1 },
+	{ "VirtualBox",          NULL,       NULL,       1 << 3,            True,        -1 },
+	{ "OpenOffice.org 3.1",  NULL,       NULL,       1 << 4,            False,       -1 },
+	{ "Gimp",                NULL,       NULL,       1 << 4,            True,        -1 },
+	{ "mplayer",             NULL,       NULL,       1 << 4,            True,        -1 },
 };
 
 /* layout(s) */
