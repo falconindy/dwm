0ae21e85d9f951acd3f8e82c160e61ce612c3837
Dave Reisner <haruko@quake.localdomain>
2009-12-13 00:43:38 -0500

    Dont need chrome to float with pertag patch

--- config.h
+++ config.h
@@ -1,15 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
-/* Addons */
-#include "push.c"
-
 /* appearance */
 //static const char font[]            = "-*-terminus-medium-r-normal-*-14-*-*-*-*-*-*-*";
 static const char font[]            = "-*-bitstream vera sans mono-medium-r-normal-*-10-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#333333";
 static const char normbgcolor[]     = "#051121"; 
 static const char normfgcolor[]     = "#FFFFFF"; 
-static const char selbordercolor[]  = "#F6EE09"; /* "#3465A4"; */
+static const char selbordercolor[]  = "#F6EE09";
 static const char selbgcolor[]      = "#3465A4"; 
 static const char selfgcolor[]      = "#FFFFFF"; 
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -22,7 +19,7 @@ static const char *tags[] = { "web", "term", "pidgin", "vbox", "misc" };
 
 static const Rule rules[] = {
     /* class                 instance    title       tags mask     isfloating   monitor */
-    { "Chrome",              NULL,       NULL,       1 << 0,            True,        -1 },
+    { "Chrome",              NULL,       NULL,       1 << 0,            False,        -1 },
     { "URxvt",               NULL,       NULL,       1 << 1,            False,       -1 },
     { "Pidgin",              NULL,       NULL,       1 << 2,            True,        -1 },
     { "Pymp.pyc",            NULL,       NULL,       1 << 3,            True,        -1 },
@@ -34,6 +31,8 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+
+#include "bstack.c"
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -104,8 +103,6 @@ static Key keys[] = {
     TAGKEYS(                        XK_8,                      7)
     TAGKEYS(                        XK_9,                      8)
     { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-    { MODKEY|ControlMask,           XK_k,      pushdown,       {0} },
-    { MODKEY|ControlMask,           XK_j,      pushup,         {0} },
 };
 
 /* button definitions */
