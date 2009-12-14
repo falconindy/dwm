7461102acf2d137613f9c843d8740609e960b0e0
Dave Reisner <haruko@quake.localdomain>
2009-12-13 19:44:51 -0500

    change modkey to Mod4

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod3Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
