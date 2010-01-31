03cc2d51537f7ef52032b8e1c1659fbb44453e6c
Dave Reisner <d@falconindy.com>
2010-01-30 21:13:51 -0500

    Add rule for Dia

--- config.h
+++ config.h
@@ -19,12 +19,13 @@ static const char *tags[] = { "web", "term", "deluge", "vbox", "misc" };
 
 static const Rule rules[] = {
     /* class                 instance    title       tags mask     isfloating   monitor */
-    { "Chromium-browser",    NULL,       NULL,       1 << 0,            False,        -1 },
+    { "Chromium-browser",    NULL,       NULL,       1 << 0,            False,       -1 },
     { "URxvt",               NULL,       NULL,       1 << 1,            False,       -1 },
     { "Deluge",              NULL,       NULL,       1 << 2,            True,        -1 },
     { "VirtualBox",          NULL,       NULL,       1 << 3,            True,        -1 },
-    { "OpenOffice.org 3.1",  NULL,       NULL,       1 << 4,            False,       -1 },
+    { "Dia",                 NULL,       NULL,       1 << 4,            True,        -1 }.
     { "Gimp",                NULL,       NULL,       1 << 4,            True,        -1 },
+    { "OpenOffice.org 3.1",  NULL,       NULL,       1 << 4,            False,       -1 },
 };
 
 /* layout(s) */
